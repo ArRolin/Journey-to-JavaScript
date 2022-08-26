8528dbb00e4875b20f82ed2aa6ed50af37ff10b5
ArRolin <gmarrolin@gmail.com>
2022-08-26 17:05:10 +0600

    02 Variables Section Added

---
+++ 02_variables/scripts.js
@@ -0,0 +1,9 @@
+let welcomeMessage = "Welcome to Variables!";
+
+welcomeMessage = "Hello Message Overrite";
+
+console.log(welcomeMessage);
+
+const repoTitle = "Journey to JavaScript";
+
+console.log(repoTitle);
